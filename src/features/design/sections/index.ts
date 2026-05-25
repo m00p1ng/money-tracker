ffb4ec1e8da625e39c33198dbf692374fd055027
m00p1ng <mongkonchai4412@gmail.com>
2026-05-25 13:03:19 +0700

    feat: add barrel export for design/sections

---
+++ src/features/design/sections/index.ts
@@ -0,0 +1,4 @@
+export { FeatureSection } from './FeatureSection'
+export { SharedComponentsSection } from './SharedComponentsSection'
+export { TokensSection } from './TokensSection'
+export { UIComponentsSection } from './UIComponentsSection'
