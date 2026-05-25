afa983a188d81ed9f6da1a14d0e567596a4506e9
m00p1ng <mongkonchai4412@gmail.com>
2026-05-25 16:08:18 +0700

    feat: render Background in AppShell

--- src/components/AppShell.tsx
+++ src/components/AppShell.tsx
@@ -2,6 +2,7 @@ import cx from 'classnames'
 import type { PropsWithChildren } from 'react'
 
 import { BottomNav } from '@/components'
+import { Background } from '@/components/Background'
 
 type AppShellProps = PropsWithChildren<{
   showBottomNav?: boolean
@@ -10,6 +11,7 @@ type AppShellProps = PropsWithChildren<{
 export function AppShell({ children, showBottomNav = true }: AppShellProps) {
   return (
     <div className="min-h-screen text-slate-50">
+      <Background />
       <main
         className={cx(
           'mx-auto min-h-screen w-full max-w-107.5 px-4 pt-6',
